199951ddb63626a2d7e54f7b916954ab1ac8ab1a
raygdev <101662915+raygdev@users.noreply.github.com>
2024-10-20 16:22:17 -0400

    Create popover for logout

---
+++ src/components/pop-over/pop-over.tsx
@@ -0,0 +1,30 @@
+import { Component, h, State, Prop, Element } from "@stencil/core";
+
+@Component({
+    tag: 'pop-over',
+    styleUrl: 'pop-over.css'
+})
+
+export class PopOver {
+    @Prop() isVisible: boolean
+    @Prop() ariaLive: boolean;
+    @Element() el: HTMLElement;
+    @Prop() triggerElement: HTMLButtonElement
+    @Prop() classes: string
+    componentDidLoad() {
+        const button = this.el.querySelector('button')
+        button.focus()
+    }
+
+    disconnectedCallback() {
+        this.triggerElement.focus()
+    }
+
+    render() {
+        return (
+            <div class={this.classes} id='popover' role="dialog" >
+                <slot></slot>
+            </div>
+        )
+    }
+}
