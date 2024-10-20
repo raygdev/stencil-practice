427d71ecc3bab154145e38c67adb71662e5ebab3
raygdev <101662915+raygdev@users.noreply.github.com>
2024-10-20 16:28:03 -0400

    Use auth service and create listener

--- src/components/app-root/app-root.tsx
+++ src/components/app-root/app-root.tsx
@@ -1,4 +1,9 @@
-import { Component, h } from '@stencil/core';
+import { Component, h, State } from '@stencil/core';
+import { createRouter, Route, href, Router, match } from 'stencil-router-v2'
+import { authService } from '../../services/auth';
+import { IUser } from '../../api/users/user-service';
+
+const Router = createRouter()
 
 @Component({
   tag: 'app-root',
@@ -6,6 +11,30 @@ import { Component, h } from '@stencil/core';
   shadow: true,
 })
 export class AppRoot {
+  @State() user: IUser | null;
+  @State() isLoggedIn: boolean;
+  @State() errors: { errors: [{ message: string, field?: string}]} | null;
+
+  onAuthChange = () => {
+    this.isLoggedIn = authService.isLoggedIn()
+    this.user = authService.getUser()
+  }
+
+  setErrors = (e) => {
+    console.log("Logging", e)
+    this.errors = e
+  }
+
+  componentWillLoad() {
+    this.isLoggedIn = authService.isLoggedIn()
+    this.user = authService.getUser()
+    authService.addAuthChangeListener(this.onAuthChange)
+  }
+
+  disconnectedCallback() {
+    authService.removeAuthChangeListener(this.onAuthChange)
+  }
+
   render() {
     return (
       <div>
