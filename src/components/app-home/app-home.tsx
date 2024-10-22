7f5dad6fd737a064682ab4e3652b4569890448e8
raygdev <101662915+raygdev@users.noreply.github.com>
2024-10-21 22:05:40 -0400

    Remove broken button

--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -13,10 +13,6 @@ export class AppHome {
           Welcome to the Stencil App Starter. You can use this starter to build entire apps all with web components using Stencil! Check out our docs on{' '}
           <a href="https://stenciljs.com">stenciljs.com</a> to get started.
         </p>
-
-        <stencil-route-link url="/profile/repos">
-          <button>Profile page</button>
-        </stencil-route-link>
       </div>
     );
   }
